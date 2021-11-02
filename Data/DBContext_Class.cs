54ed6a16d6c1c0b55ec3002e0dcfa0e97641591f
Jude Akhimien <judesco247@gmail.com>
2021-11-02 02:10:21 +0100

    Adding a model and Adding initial Migration and Updating of database

---
+++ Data/DBContext_Class.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentData.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentData.Data
+{
+    public class DBContext_Class
+    {
+        public class StudentDataContext : DbContext
+        {
+            public StudentDataContext(DbContextOptions<StudentDataContext> options) : base(options)
+            {
+            }
+
+            public DbSet<StudentReg> StudentReg { get; set; }
+        }
+    }
+}
---
+++ Migrations/20211102010655_InitialMigration.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StudentData.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "StudentReg",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: true),
+                    Surname = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: true),
+                    LastName = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: true),
+                    Address = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    StateOfOrigin = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: true),
+                    LGA = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: true),
+                    Religion = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: true),
+                    DateOfBirth = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    PhoneNumber = table.Column<int>(type: "int", nullable: false),
+                    Alt_PhoneNumber = table.Column<int>(type: "int", nullable: false),
+                    Program = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Department = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Course = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DateOfAdmission = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StudentReg", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "StudentReg");
+        }
+    }
+}
